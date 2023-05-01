4b50ac9cc363ff84c91b8876546b994054b92117
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-05-01 14:41:52 +0300

    add animationto headings on hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import Hero from './Components/Hero'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 export default function App() {
   return (
+    <>
     <NavBar />
+    <Hero />
+    </>
   )
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div class="text-secondary px-4 py-5 text-center">
+    <div class="py-5">
+    <div class="typewriter">
+      <h1 class="display-5 fw-bold text-white">Hi, my name is Zahid.</h1>
+    </div>
+    <div class="typewriter">
+    <h1 class="display-5 fw-bold" style={{color: 'aqua'}}>I am a Full Stack Developer.</h1>
+    </div>
+      <div class="col-lg-6 mx-auto">
+        <p class="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
+          <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
