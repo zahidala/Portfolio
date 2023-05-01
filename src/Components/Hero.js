a169db9d95a389ba3f9806aee36d75771bd6eb5a
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-05-01 17:36:17 +0300

    fix hero heading animation a little

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,11 +4,13 @@ export default function Hero() {
   return (
     <div class="text-secondary px-4 py-5 text-center">
     <div class="py-5">
+    <div class="col-lg-6 mx-auto">
     <div class="typewriter">
       <h1 class="display-5 fw-bold text-white">Hi, my name is Zahid.</h1>
     </div>
     <div class="typewriter">
     <h1 class="display-5 fw-bold" style={{color: 'aqua'}}>I am a Full Stack Developer.</h1>
+    </div>
     </div>
       <div class="col-lg-6 mx-auto">
         <p class="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
