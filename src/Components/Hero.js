d12de132bf7b8fa33f7428120e582124da0bb4cf
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-05-02 20:16:24 +0300

    Change hero section buttons

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 
 export default function Hero() {
   return (
@@ -16,8 +17,9 @@ export default function Hero() {
             <div className="col-lg-6 mx-auto">
                 <p className="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-                    <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button>
+                    {/* <button type="button" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button> */}
+                    {/* <button type="button" className="btn btn-outline-light btn-lg px-4">Secondary</button> */}
+                    <Button href="https://linkedin.com/in/zahidallaulddin" target="_blank" variant="info"><span id="connect" className="fw-semibold">Let's Connect</span></Button>{' '}
                 </div>
               </div>
           </div>
